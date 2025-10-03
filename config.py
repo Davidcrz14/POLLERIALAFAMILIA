3ab52e13ad994d61e21ab9f0de578a579e32a2ef
DavC | Daizen <70966976+Davidcrz14@users.noreply.github.com>
2025-10-03 12:58:06 -0600

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ DB_NAME = "polleria"
 API_URL= "https://pollerialafamilia.com"
 
 # Configuración de OpenAI
-OPENAI_API_KEY = "sk-proj-XQRD3G4wBIYZqeUtCaS6T3BlbkFJwKGYY5oYe9ef4zKW679G"
+OPENAI_API_KEY = "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"
